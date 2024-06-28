f870e22d305506fe9d67af65c2fb1177850385b1
Dave Lim <davelim77@gmail.com>
2024-06-28 15:21:29 -0500

    Plumbing for persistent data store (MySQL)
    1. Models/ApplicationUser.cs: POCO for user
    2. Data/*:
      - updates for MySQL & auth stuff
    3. backend.csproj, appsettings.json, Program.cs
      - packages for MySQL (& Identity)
      - use secrets & appsettings.json

---
+++ backend/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace backend.models;
+
+public class ApplicationUser: IdentityUser
+{
+}
+
--- backend/Program.cs
+++ backend/Program.cs
@@ -4,13 +4,20 @@ using backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration["StlMarkets:ConnectionString"];
+var Major = builder.Configuration.GetValue<int>("MySQL:Major");
+var Minor = builder.Configuration.GetValue<int>("MySQL:Minor");
+var Build = builder.Configuration.GetValue<int>("MySQL:Build");
+var serverVersion = new MySqlServerVersion(
+    new Version(Major, Minor, Build));
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseMySql(connectionString, serverVersion));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(
+      options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
