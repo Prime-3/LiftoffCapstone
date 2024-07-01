d9a9b0d22d60e8e850e7b9a97aaed4d2de3faaa5
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-01 15:10:27 -0500

    added basic header

---
+++ frontend/src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+   return (
+      <>
+         <header>
+            <img src="../images/STLmarkets.png"></img>
+            <form>
+               <input type="text"></input>
+               <button>Go</button>
+            </form>
+            <button>Profile</button>
+         </header>
+      </>
+   )
+}
+
+export default Header;
