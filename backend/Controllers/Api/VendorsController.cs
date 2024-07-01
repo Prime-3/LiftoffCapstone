dc2946e295a094dcfc68a51ce07db8edc7d965fc
Elizabeth Levinson <lizarose266@gmail.com>
2024-07-01 15:57:26 -0500

    Started the Vendors api controller, class and context created

--- backend/Controllers/Api/VendorsController.cs
+++ backend/Controllers/Api/VendorsController.cs
@@ -1,4 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using backend.Data;
 
-namespace backend.Controllers;
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VendorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }  
+    }
+}
