9fb093d74ad27be845d7d249972d351ccf237a85
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-02 14:15:37 -0500

    created login modal

---
+++ frontend/src/Components/LoginFormModal/index.jsx
@@ -0,0 +1,22 @@
+
+
+function LoginFormModal({ onClose }) {
+   return (
+      <div className="modal-background" onClick={onClose}>
+         <div className="modal">
+
+            <form className="login-form">
+               <h1>Login</h1>
+               <label>Username</label>
+               <input type="text"></input>
+               <lable>Password</lable>
+               <input type="password"></input>
+               <button onClick={onClose}>Close</button>
+            </form>
+         </div>
+
+      </div>
+   );
+}
+
+export default LoginFormModal;
