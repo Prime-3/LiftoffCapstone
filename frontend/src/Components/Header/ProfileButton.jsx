7509a2ba9981973ee39e4550f7f4b275cb5c221b
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-01 22:58:06 -0500

    created profile button

---
+++ frontend/src/Components/Header/ProfileButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfileButton = () => {
+   return <>
+      <button className="profile-button">
+         <p>Login</p>
+         {/* <i class="fa-solid fa-circle-user fa-2xl"></i> */}
+         <i className="fa-solid fa-bars fa-lg" />
+      </button>
+   </>
+}
+export default ProfileButton;
