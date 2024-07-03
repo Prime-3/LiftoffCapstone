368bb423d98b641b0d9497b32aed3c6915f522f7
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-02 19:43:15 -0500

    added minor required validations

--- frontend/src/Components/RegisterFormModal/index.jsx
+++ frontend/src/Components/RegisterFormModal/index.jsx
@@ -13,32 +13,33 @@ function RegisterFormModal({ onClose }) {
             <form className="login-form">
                <h1>Register</h1>
                <label>First Name</label>
-               <input type="text"></input>
+               <input type="text" required></input>
                <label>Last Name</label>
-               <input type="text"></input>
+               <input type="text" required></input>
                <label>Email</label>
-               <input type="email"></input>
+               <input type="email" required></input>
                <lable>Password</lable>
-               <input type="password"></input>
+               <input type="password" required></input>
                <lable>Confirm Password</lable>
-               <input type="password"></input>
+               <input type="password" required></input>
                <input type="checkbox" onClick={handleClick}></input>
                <span>Vendor</span>
                {isVendor && (
                   <>
                      <label>Vender Name</label>
-                     <input type="text"></input>
+                     <input type="text" required></input>
                      <label>Phone #</label>
                      <input type="text"></input>
                      <label>Category</label>
-                     <select>
+                     <select required>
+                        <option value="">Select One</option>
                         <option value="test1">test1</option>
                         <option value="test2">test2</option>
                         <option value="test3">test3</option>
                      </select>
                   </>
                )}
-               <button onClick={onClose}>Close</button>
+               <button type="submit" >Submit</button>
             </form>
          </div>
       </div>
