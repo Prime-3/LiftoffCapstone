ada0ae2daafe90cb14d9ad5ffa73d274cd45c760
Elizabeth Levinson <lizarose266@gmail.com>
2024-07-03 14:09:02 -0500

    Added Patch to update existing vendor info, removed registration from Post route

--- backend/Controllers/Api/VendorsController.cs
+++ backend/Controllers/Api/VendorsController.cs
@@ -49,8 +49,8 @@ namespace backend.Controllers;
         }
 
 
-        // POST: api/vendors/registration --> Posts new vendor registration info
-        [HttpPost("registration")]
+        // POST: api/vendors --> Posts new vendor registration info
+        [HttpPost]
         public IActionResult RegisterVendor([FromBody] Vendors vendor) 
         {
             if (ModelState.IsValid)
@@ -69,4 +69,36 @@ namespace backend.Controllers;
         }
 
 
+        // PATCH: api/vendors/{id} --> Patch updates existing vendor information
+        [HttpPatch("{id}")]
+        public IActionResult UpdateVendor(int id, [FromBody] Vendors vendors)
+        {
+            //Get existing vendor info
+            var existingVendor = context.Vendors.FirstOrDefault(v => v.Id == id);
+            if (existingVendor == null)
+            {
+                return NotFound();
+            }
+
+            //Apply changes
+                //If vendors is not null and does not equal existingVendor then update
+            existingVendor.ShopName = (vendors.ShopName != null) && (vendors.ShopName != existingVendor.ShopName) ? vendors.ShopName : existingVendor.ShopName;
+
+            existingVendor.OwnerName = (vendors.OwnerName != null) && (vendors.OwnerName != existingVendor.OwnerName) ? vendors.OwnerName : existingVendor.OwnerName;
+
+            existingVendor.Description = (vendors.Description != null) && (vendors.Description != existingVendor.Description) ? vendors.Description : existingVendor.Description;
+
+            existingVendor.PhoneNumber = (vendors.PhoneNumber != null) && (vendors.PhoneNumber != existingVendor.PhoneNumber) ? vendors.PhoneNumber : existingVendor.PhoneNumber;
+
+            existingVendor.Address = (vendors.Address != null) && (vendors.Address != existingVendor.Address) ? vendors.Address : existingVendor.Address;
+
+            existingVendor.Website = (vendors.Website != null) && (vendors.Website != existingVendor.Website) ? vendors.Website : existingVendor.Website;
+
+
+            //Save changes
+            context.SaveChanges();
+            
+            //Return response
+            return Ok(new {message = "Account information has successfully been updated."});
+        }
     }
