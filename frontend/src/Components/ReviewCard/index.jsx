f1558c40772c3c4346ab93f5ba61c9c84c8df704
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-07 13:46:12 -0500

    added styles

--- frontend/src/Components/ReviewCard/index.jsx
+++ frontend/src/Components/ReviewCard/index.jsx
@@ -1,12 +1,16 @@
 import "./ReviewCard.css"
 
 function ReviewCard() {
+   // TODO: Add parms to pass review data into card
+   let reviewTitle = "This place is awesome!"
    let stars = 3;
+   let reviewedBy = "Joe Schmoe"
+   let reviewComment = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci!"
 
    return (
       <div className="review-card">
-         <div>
-            <h4>Review</h4>
+         <div id="title-stars">
+            <h3>{reviewTitle}</h3>
             <div className="stars">
                <i
                   className={(stars >= 1) ? "fa-solid fa-star" : "fa-regular fa-star"}
@@ -25,8 +29,8 @@ function ReviewCard() {
                ></i>
             </div>
          </div>
-         <h5>Review by UserName</h5>
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem optio enim delectus et fugiat unde animi laudantium quos esse. Ex amet illo suscipit maiores at magnam fugiat temporibus impedit adipisci!</p>
+         <h5 id="reviewed-by">Review by {reviewedBy}</h5>
+         <p id="review-comment">{reviewComment}</p>
       </div>
    )
 }
