b9dc95ecdd266a91a97187485015b3f59087fac5
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-08 19:28:23 -0500

    added post to register user account

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,4 +1,3 @@
-import Header from "../Components/Header";
 import ReviewCard from "../Components/ReviewCard";
 import CreateReview from "../Components/CreateReview";
 import Header from "../Components/Header"
