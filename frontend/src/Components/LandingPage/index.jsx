a937d540d14171842c25408f2392031b21dbc575
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-10 00:05:22 -0500

    add some styles

--- frontend/src/Components/LandingPage/index.jsx
+++ frontend/src/Components/LandingPage/index.jsx
@@ -1,29 +1,29 @@
-
+import "./LandingPage.css"
 
 const LandingPage = () => {
    return (
-      <>
-         <h1>This is the Landing Page. Something short about the app.</h1>
-         <button>Browse Shops</button>
+      <div id="landing-page">
+         <h1 id="slogan">This is the Landing Page. Something short about the app. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit.</h1>
+         <button className="submit-button">Browse Shops</button>
          <h2>How to use:</h2>
-         <div>
-            <div>
-               <img src="images/dummy-image-square.png"></img>
+         <div id="instructions">
+            <div className="intro-cards">
+               <img className="intro-photos" src="images/dummy-image-square.png"></img>
                <h5>Subheading</h5>
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
             </div>
-            <div>
-               <img src="images/dummy-image-square.png"></img>
+            <div className="intro-cards">
+               <img className="intro-photos" src="images/dummy-image-square.png"></img>
                <h5>Subheading</h5>
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
             </div>
-            <div>
-               <img src="images/dummy-image-square.png"></img>
+            <div className="intro-cards">
+               <img className="intro-photos" src="images/dummy-image-square.png"></img>
                <h5>Subheading</h5>
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
             </div>
          </div>
-      </>
+      </div>
    )
 }
 
