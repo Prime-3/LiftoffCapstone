638630711408550315316f8400bb9fed18df3521
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-09 23:26:28 -0500

    created landing page component

---
+++ frontend/src/Components/LandingPage/index.jsx
@@ -0,0 +1,11 @@
+
+
+const LandingPage = () => {
+   return (
+      <>
+         <h1>This is the Landing Page</h1>
+      </>
+   )
+}
+
+export default LandingPage;
--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -3,45 +3,14 @@ import CreateReview from "../Components/CreateReview";
 import Header from "../Components/Header"
 import SearchCard from "../Components/SearchCard";
 import { useState, useEffect } from "react";
+import LandingPage from "../Components/LandingPage";
 
 export default function Root() {
 
     return (
         <>
             <Header />
-
-            {/* For testing review cards
-            <CreateReview />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard /> */}
-
-            {/* Uncomment to test
-            <div id="cards">
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-            </div>
-            <div id="cards">
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-            </div>
-            <div id="cards">
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-            </div>
-            <div id="cards">
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-            </div> */}
+            <LandingPage />
         </>
     )
 }
