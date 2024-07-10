e2148981085d9405ed81ac95039a0e5d1eb4f74f
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-09 23:39:19 -0500

    added basic structure html

--- frontend/src/Components/LandingPage/index.jsx
+++ frontend/src/Components/LandingPage/index.jsx
@@ -3,7 +3,26 @@
 const LandingPage = () => {
    return (
       <>
-         <h1>This is the Landing Page</h1>
+         <h1>This is the Landing Page. Something short about the app.</h1>
+         <button>Browse Shops</button>
+         <h2>How to use:</h2>
+         <div>
+            <div>
+               <img src="images/dummy-image-square.png"></img>
+               <h5>Subheading</h5>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
+            </div>
+            <div>
+               <img src="images/dummy-image-square.png"></img>
+               <h5>Subheading</h5>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
+            </div>
+            <div>
+               <img src="images/dummy-image-square.png"></img>
+               <h5>Subheading</h5>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iste, nobis deleniti alias asperiores saepe nisi fuga sit, fugit magnam assumenda, et natus adipisci? Aperiam quam quod eos nihil tenetur.</p>
+            </div>
+         </div>
       </>
    )
 }
