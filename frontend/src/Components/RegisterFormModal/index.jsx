a04bd2128acf30ac1d37206ddd92ce3ecd563754
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-10 23:15:23 -0500

    add styles

--- frontend/src/Components/RegisterFormModal/index.jsx
+++ frontend/src/Components/RegisterFormModal/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./RegisterFormModal.css"
 
 
 function RegisterFormModal({ onClose }) {
@@ -120,68 +121,72 @@ function RegisterFormModal({ onClose }) {
          <div className="modal-background" onClick={onClose}></div>
          <div className="modal-content">
 
-            <form className="login-form" >
-               <h2 className="form-title">Register Account</h2>
-               <label>First Name</label>
-               <input
-                  type="text"
-                  name="firstName"
-                  onChange={handleChange}
-                  required />
-               <label>Last Name</label>
-               <input
-                  type="text"
-                  name="lastName"
-                  onChange={handleChange}
-                  required />
-               <label>Email</label>
-               <input
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                  required />
-               <label>Password</label>
-               <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  onChange={handleChange}
-                  required />
-               <label>Confirm Password</label>
-               <input
-                  type="password"
-                  id="password-confirm"
-                  onInput={passwordCheck}
-                  required />
-               {error && <span className="error">{error}</span>}
-               <div id="vendor-checkbox-area">
-                  <input type="checkbox" id="vendor-checkbox" onClick={handleCheck}></input>
-                  <label>Are you a vendor?</label>
+            <h2 className="form-title">Register Account</h2>
+            <form className="login-form">
+               <div id="register-form">
+                  <div id="account-container">
+                     <label>First Name</label>
+                     <input
+                        type="text"
+                        name="firstName"
+                        onChange={handleChange}
+                        required />
+                     <label>Last Name</label>
+                     <input
+                        type="text"
+                        name="lastName"
+                        onChange={handleChange}
+                        required />
+                     <label>Email</label>
+                     <input
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        required />
+                     <label>Password</label>
+                     <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        onChange={handleChange}
+                        required />
+                     <label>Confirm Password</label>
+                     <input
+                        type="password"
+                        id="password-confirm"
+                        onInput={passwordCheck}
+                        required />
+                     {error && <span className="error">{error}</span>}
+                     <div id="vendor-checkbox-area">
+                        <input type="checkbox" id="vendor-checkbox" onClick={handleCheck}></input>
+                        <label>Are you a vendor?</label>
+                     </div>
+                  </div>
+                  {isVendor && (
+                     <div id="shop-container">
+                        <label>Shop Name</label>
+                        <input
+                           type="text"
+                           name="shopName"
+                           onChange={handleChange}
+                           required />
+                        <label>Phone #</label>
+                        <input
+                           type="text"
+                           name="phoneNum"
+                           onChange={handleChange}
+                           required />
+                        <label>Category</label>
+                        <select
+                           onChange={handleChange}
+                           name="category"
+                           required>
+                           <option value="">Select One</option>
+                           {categories.map(c => <option value={c}>{c}</option>)}
+                        </select>
+                     </div>
+                  )}
                </div>
-               {isVendor && (
-                  <>
-                     <label>Shop Name</label>
-                     <input
-                        type="text"
-                        name="shopName"
-                        onChange={handleChange}
-                        required />
-                     <label>Phone #</label>
-                     <input
-                        type="text"
-                        name="phoneNum"
-                        onChange={handleChange}
-                        required />
-                     <label>Category</label>
-                     <select
-                        onChange={handleChange}
-                        name="category"
-                        required>
-                        <option value="">Select One</option>
-                        {categories.map(c => <option value={c}>{c}</option>)}
-                     </select>
-                  </>
-               )}
                <button type="submit" className="submit-button" onClick={handleSubmit}>Submit</button>
             </form>
          </div>
