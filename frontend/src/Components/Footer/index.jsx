5d99b8d36fb0996c6aca0e076c963e6c4ef0f67d
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-15 12:01:30 -0500

    Created basic structure

---
+++ frontend/src/Components/Footer/index.jsx
@@ -0,0 +1,32 @@
+import "./Footer.css"
+
+const Footer = () => {
+   return (
+      <div id="footer-container">
+         <div>
+            <h1>FooterTitle</h1>
+            <div>
+               <i class="fa-brands fa-facebook"></i>
+               <i class="fa-brands fa-instagram"></i>
+               <i class="fa-brands fa-x-twitter"></i>
+            </div>
+         </div>
+         <ul>
+            <li>
+               <a href="/">Link</a>
+            </li>
+            <li>
+               <a href="/">Link</a>
+            </li>
+            <li>
+               <a href="/">Link</a>
+            </li>
+            <li>
+               <a href="/">Link</a>
+            </li>
+         </ul>
+      </div>
+   )
+}
+
+export default Footer;
