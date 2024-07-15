a71fbfe5bc78829d9dd551f4a1cfb9ab1bf14054
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-15 15:14:36 -0500

    Footer completed

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -3,27 +3,36 @@ import "./Footer.css"
 const Footer = () => {
    return (
       <div id="footer-container">
-         <div>
-            <h1>FooterTitle</h1>
+         <div id="footer-title-socials">
             <div>
-               <i class="fa-brands fa-facebook"></i>
-               <i class="fa-brands fa-instagram"></i>
-               <i class="fa-brands fa-x-twitter"></i>
+               <h2>STL Markets&#8482;</h2>
+            </div>
+            <div id="footer-socials">
+               <a href="/"><i class="fa-brands fa-facebook" /></a>
+               <a href="/"><i class="fa-brands fa-instagram" /></a>
+               <a href="/"><i class="fa-brands fa-x-twitter" /></a>
             </div>
          </div>
-         <ul>
-            <li>
-               <a href="/">Link</a>
-            </li>
-            <li>
-               <a href="/">Link</a>
-            </li>
-            <li>
-               <a href="/">Link</a>
-            </li>
-            <li>
-               <a href="/">Link</a>
-            </li>
+         <ul id="site-map">
+            <h5>Site Map</h5>
+            <div id="site-map-links">
+               <div>
+                  <li>
+                     <a href="/">Home</a>
+                  </li>
+                  <li>
+                     <a href="/">Account</a>
+                  </li>
+               </div>
+               <div>
+                  <li>
+                     <a href="/browse">Browse</a>
+                  </li>
+                  <li>
+                     <a href="/">Contact</a>
+                  </li>
+               </div>
+            </div>
          </ul>
       </div>
    )
