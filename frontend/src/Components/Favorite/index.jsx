c036f6ca321fc3df687f6c440adbd572d8545175
Elizabeth Levinson <lizarose266@gmail.com>
2024-07-16 11:43:04 -0500

    Favorite List is working and has styling

--- frontend/src/Components/Favorite/index.jsx
+++ frontend/src/Components/Favorite/index.jsx
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from "react";
 import FavoriteButton from "./FavoriteButton"; 
 import { useParams } from "react-router-dom";
+import "./favoriteStyles.css" 
 
 const Favorited = () => {
     const [favorites, setFavorites] = useState([]);
     const [userId, setUserId] = useState("");
-    // let { userId } = useParams();
 
   // get userId
-//   useEffect(() => {
-//     fetch("/pingauth")
-//     .then((resp) => {
-//       if (resp.ok) {
-//         return resp.json();
-//       }
-//       else {
-//         return null;
-//       }
-//     })
-//     .then((data) => {
-//         setUserId(data.userId);
-//         console.log(`My userid : ${data.userId}`);
-//         console.log(data);
-//     })
-//   }, []);
-
-  //fetch user favorites list
   useEffect(() => {
     fetch("/pingauth")
     .then((resp) => {
@@ -37,19 +19,9 @@ const Favorited = () => {
       }
     })
     .then((data) => {
-        // setUserId(data.userId);
-        // console.log(`My userid : ${data.userId}`);
-        // console.log(data);
         return data.userId
     })
-    // console.log(`my user id : '${userId}'`)
-    // console.log(typeof userId);
-    // if (userId =="") {
-    //     setFavorites([]);
-    //     return () => {};
-    // }
-    // const url = `/api/favorites/${userId}`;
-    // console.log(`my url: ${url}`);
+    //fetch user favorites list
     .then((userId) => {
         console.log("User id: ", userId)
         fetch(`/api/favorites/${userId}`, {
@@ -59,14 +31,6 @@ const Favorited = () => {
             }, 
         })
         .then((resp) => {
-            console.log(resp);
-            // if (resp.ok) {
-            //     console.log("ok!");
-            //     return resp.json();
-            // }
-            // return [];
-            // console.log(data)
-            // setFavorites(data)
             return resp.json()
         })
         .then((data) => {
@@ -80,56 +44,60 @@ const Favorited = () => {
 
 
 
-//click to add or remove
-const handleFavoriteButton = (vendorId) => {
-    console.log("HIT handlesubmit")
-    const isFavorited = favorites.includes(vendorId)
+// //click to add or remove
+// const handleFavoriteButton = (vendorId) => {
+//     console.log("HIT handlesubmit")
+//     const isFavorited = favorites.includes(vendorId)
 
-    if(isFavorited) {
-        //remove from favorites
-        fetch(`/api/favorites/remove`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userId: userId, 
-            }),
-        })
-        .then((resp) => {
-            console.log(resp);
-        })
-    }else {
-        //add to favorites
-        fetch(`/api/favorites/add`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userId: userId,
-            }),
-        })
-        .then((resp) => {
-            console.log(resp);
-        })
-    }
+//     if(isFavorited) {
+//         //remove from favorites
+//         fetch(`/api/favorites/remove`, {
+//             method: 'DELETE',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({
+//                 userId: userId, 
+//             }),
+//         })
+//         .then((resp) => {
+//             console.log(resp);
+//         })
+//     }else {
+//         //add to favorites
+//         fetch(`/api/favorites/add`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({
+//                 userId: userId,
+//             }),
+//         })
+//         .then((resp) => {
+//             console.log(resp);
+//         })
+    // }
     
     
-}
+// }
   return (
-    <div>
-        <h1>TEST</h1>
+    <div className="favoriteList">
+        <h1 className="title">Favorites List</h1>
         {/* map over each favorited vendor and display them  */}
         {favorites.map((vendor) => (        
-            <div key = {vendor.ShopId}>
-                <div>{vendor.ShopName}</div>
-                <div>{vendor.Description}</div>
+            <div className="key" key = {vendor.shopId}>
+                <div className="logo"><img className="format" src={vendor.logo} /></div>
+                <div>
+                <div className="shopname">{vendor.shopName} </div>
+                <div className="description">{vendor.description}</div>
+                </div>
+                {/* <FavoriteButton className= "button" onClick={(vendorId) => handleFavoriteButton(vendorId)} /> */}
             </div>
-            ))};      
-      <FavoriteButton onClick={(vendorId) => handleFavoriteButton(vendorId)} />
+            ))}  
+     
     </div>
-  );
+  )
 }
 
 export default Favorited;
