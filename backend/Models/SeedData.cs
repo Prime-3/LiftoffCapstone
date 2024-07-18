16f2665339b6b98166bc7643f5937f825f7bf8f4
Dave Lim <davelim77@gmail.com>
2024-07-18 16:52:59 -0500

    Seed our persistent store with admin role and user
    On startup, app adds a user and adminstrator role to persistent Identity
    store.
    - Administrator user:
      - first name: Admin
      - last name: Istrator
      - email: stlmarkets7@gmail.com
      - username: stlmarkets7@gmail.com
    - Administrator role is "Administrator".
    Do note, app now requires a "SeedUserPW" variable in user secrets that
    holds a "strong" enough password.

---
+++ backend/Models/SeedData.cs
@@ -0,0 +1,79 @@
+// https://learn.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-8.0
+// https://stackoverflow.com/questions/34343599/how-to-seed-users-and-roles-with-code-first-migration-using-identity-asp-net-cor
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using backend.Data;
+
+namespace backend.Models;
+
+public class SeedData
+{
+    public static async Task InitializeAsync(IServiceProvider serviceProvider, string testUserPw)
+    {
+        using (var context = new ApplicationDbContext(
+            serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+        {
+            var adminID = await EnsureUser(serviceProvider, testUserPw, "stlmarkets7@gmail.com");
+            await EnsureRole(serviceProvider, adminID, "Administrator");
+        }
+    }
+    private static async Task<string> EnsureUser(IServiceProvider serviceProvider, string testUserPw, string userName)
+    {
+        var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+        if (userManager == null)
+        {
+            throw new Exception("EnsureUser: userManager null.");
+        }
+        var user = await userManager.FindByNameAsync(userName);
+        if (user == null)
+        {
+            user = new ApplicationUser
+                {
+                    FirstName = "Admin",
+                    LastName = "Istrator",
+                    Email = userName,
+                    NormalizedEmail = userName.ToUpper(),
+                    UserName = userName,
+                    NormalizedUserName = userName.ToUpper(),
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D")
+                };
+            await userManager.CreateAsync(user, testUserPw);
+        }
+        if (user == null)
+        {
+            throw new Exception("Could not add user, the password is probably not string enough.");
+        }
+        return user.Id;
+    }
+    private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider, string uid, string role)
+    {
+        var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+        if (roleManager == null)
+        {
+            throw new Exception("EnsureRole: roleManager null.");
+        }
+        IdentityResult result;
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            result = await roleManager.CreateAsync(new IdentityRole(role));
+            if (!result.Succeeded)
+            {
+                throw new Exception($"EnsureRole: could not create role ({result})");
+            }
+        }
+        var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+        if (userManager == null)
+        {
+            throw new Exception("EnsureRole: userManager null.");
+        }
+        var user = await userManager.FindByIdAsync(uid);
+        if (user == null)
+        {
+            throw new Exception("Could not find user, the password is probably not strong enough.");
+        }
+        result = await userManager.AddToRoleAsync(user, role);
+        return result;
+    }
+}
