dd88a599c4ade3c1e189d9467cce49186df97c9b
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-19 11:23:19 -0500

    added map to display images

--- frontend/src/Components/ImageGallery/index.jsx
+++ frontend/src/Components/ImageGallery/index.jsx
@@ -5,6 +5,18 @@ import "./ImageGallery.scss"
 const ImageGallery = () => {
    const [initialized, setInitialized] = useState(false)
 
+   const testImages = [
+      "https://mediaproxy.salon.com/width/1200/https://media2.salon.com/2021/08/farmers-market-produce-0812211.jpg",
+      "https://www.a2gov.org/departments/Parks-Recreation/parks-places/farmers-market/PublishingImages/Pages/default/market%20sign%20flowers.jpg?RenditionId=18",
+      "https://media.ksdk.com/assets/KSDK/images/2bc65bd9-9f20-421d-93ec-cced21c205b7/2bc65bd9-9f20-421d-93ec-cced21c205b7_1920x1080.jpg",
+      "https://www.sftravel.com/sites/default/files/styles/hero/public/2022-11/farmers-market-tomatoes.jpg.webp?itok=camAgJHT",
+      "https://thepaleodiet.com/wp-content/uploads/2023/05/woman-selling-produce-at-farmers-market.jpg",
+      "https://friscofreshmarket.com/wp-content/uploads/2022/09/fall-farmers-market-scaled.jpeg",
+      "https://www.aldenestatesofjefferson.com/wp-content/uploads/sites/63/2019/08/GettyImages-671270196-1-600x511.jpg",
+      "https://www.socosy.co.uk/wp-content/uploads/2020/07/farmers-market.webp",
+      "https://img1.wsimg.com/isteam/stock/26336/:/rs=w:1280",
+   ]
+
    useEffect((e) => {
       console.log("useEffect HIT")
       // document.addEventListener("load", function () {
@@ -12,27 +24,27 @@ const ImageGallery = () => {
       let items = carousel.querySelectorAll(".item");
       let dotsContainer = document.querySelector(".dots");
 
-      if (!initialized) {
-         // Insert dots into the DOM
-         items.forEach((index) => {
-            let dot = document.createElement("span");
-            dot.classList.add("dot");
-            if (index === 0) dot.classList.add("active");
-            dot.dataset.index = index;
-            dotsContainer.appendChild(dot);
-         });
-         setInitialized(true)
-      }
-      let dots = document.querySelectorAll(".dot");
+      // if (!initialized) {
+      // Insert dots into the DOM
+      //    items.forEach((index) => {
+      //       let dot = document.createElement("span");
+      //       dot.classList.add("dot");
+      //       if (index === 0) dot.classList.add("active");
+      //       dot.dataset.index = index;
+      //       dotsContainer.appendChild(dot);
+      //    });
+      //    setInitialized(true)
+      // }
+      // let dots = document.querySelectorAll(".dot");
 
       // Function to show a specific item
       function showItem(index) {
          items.forEach((item, idx) => {
             item.classList.remove("active");
-            dots[idx].classList.remove("active");
+            // dots[idx].classList.remove("active");
             if (idx === index) {
                item.classList.add("active");
-               dots[idx].classList.add("active");
+               // dots[idx].classList.add("active");
             }
          });
       }
@@ -53,33 +65,28 @@ const ImageGallery = () => {
       });
 
       // Event listeners for dots
-      dots.forEach((dot) => {
-         dot.addEventListener("click", () => {
-            let index = parseInt(dot.dataset.index);
-            showItem(index);
-         });
-      });
+      // dots.forEach((dot) => {
+      //    dot.addEventListener("click", () => {
+      //       let index = parseInt(dot.dataset.index);
+      //       showItem(index);
+      //    });
+      // });
    }, [])
 
    return (
       <main class="carousel-container">
          <div class="carousel">
             <div class="item active">
-               <img src="https://bit.ly/34xczKy" alt="Image 1" />
-               <p class="caption">Caption for Image 1</p>
+               <img src={testImages[0]} />
             </div>
-            <div class="item">
-               <img src="https://bit.ly/3lkp5DW" alt="Image 2" />
-               <p class="caption">Caption for Image 2</p>
-            </div>
-            <div class="item">
-               <img src="https://bit.ly/3iMHuI1" alt="Image 3" />
-               <p class="caption">Caption for Image 3</p>
-            </div>
+            {testImages.slice(1).map((img) => (
+               <div class="item">
+                  <img src={img} />
+               </div>
+            ))}
          </div>
          <button class="btn prev">Prev</button>
          <button class="btn next">Next</button>
-         <div class="dots"></div>
       </main>
    )
 }
