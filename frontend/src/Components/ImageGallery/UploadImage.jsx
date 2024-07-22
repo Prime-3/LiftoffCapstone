f8a781e3e50b45c17ea8d8e245076a109254b9a6
dhamiltonstl <125328155+dhamiltonstl@users.noreply.github.com>
2024-07-22 15:35:20 -0500

    still trying to get the file path to the back-end

--- frontend/src/Components/ImageGallery/UploadImage.jsx
+++ frontend/src/Components/ImageGallery/UploadImage.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 
 const UploadImage = ({ shop }) => {
    const [fileToUpload, setFileToUpload] = useState(null)
+   const inputRef = useRef()
+
+
    const handleChange = (e) => {
-      const image = e.target.files[0]
+      const image = inputRef.current.files[0]
       const src = window.URL.createObjectURL(image)
       setFileToUpload(src)
-      console.log(image)
+      console.log(src)
+
    }
    const handleSubmit = () => {
       fetch("/api/photos/albums/media", {
          method: "POST",
          header: {
-            "Content-Type": "multipart/form-data"
+            "Content-Type": "application/json"
          },
          body: JSON.stringify({
             shopId: shop.id,
@@ -24,14 +28,16 @@ const UploadImage = ({ shop }) => {
    }
    return (
       <>
+         <img src={fileToUpload} />
          <input
             type="file"
             id="gallery-upload"
             name="gallery-upload"
             accept="image/*"
             onChange={handleChange}
+            ref={inputRef}
          />
-         <button onClick={handleSubmit}>Add</button>
+         <button onClick={handleSubmit}>Upload</button>
       </>
    )
 }
